101a230581e68b8b7f3b002f9ae92087a186adca
Diego Augusto <diih_augusto@live.com>
2018-07-23 22:07:38 -0300

    calculando distancia

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -9,28 +9,44 @@ class PropertyController {
    * Show a list of all properties.
    * GET properties
    */
-  async index () {
-    const properties = Property.all()
-  
-    return properties
+  async index ({ request }) {
+    const { latitude, longitude } = request.all();
+
+    const properties = Property.query()
+      .nearBy(latitude, longitude, 10)
+      .fetch();
+
+    return properties;
   }
 
   /**
    * Create/save a new property.
    * POST properties
    */
-  async store ({ request, response }) {
+  async store ({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
   }
 
   /**
    * Display a single property.
    * GET properties/:id
    */
-  async show ({ params }) {
+  async show({ params }) {
     const property = await Property.findOrFail(params.id)
-  
+
     await property.load('images')
-  
+
     return property
   }
 
@@ -38,20 +54,20 @@ class PropertyController {
    * Update property details.
    * PUT or PATCH properties/:id
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
   }
 
   /**
    * Delete a property with id.
    * DELETE properties/:id
    */
-  async destroy ({ params, auth, response }) {
+  async destroy({ params, auth, response }) {
     const property = await Property.findOrFail(params.id)
-  
+
     if (property.user_id !== auth.user.id) {
       return response.status(401).send({ error: 'Not authorized' })
     }
-  
+
     await property.delete()
   }
 }
